1429f38c04035fb75f52f0857beb5d7444f21173
tanson <tanson01657715766@gmail.com>
2024-05-04 21:28:17 +0700

    Create and code feature-Location 4/5 by devSon

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -3,27 +3,6 @@ export const ACCESS_TOKEN = 'accessToken'
 export const BASE_URL = 'https://airbnbnew.cybersoft.edu.vn'
 export const ACCESS_USER_ID = 'accessUserById'
 export const PAGE_SIZE = 2; 
-export interface IRoomDetail {
-    id: number,
-    tenPhong: string,
-    khach: number,
-    phongNgu: number,
-    giuong: number,
-    phongTam: number,
-    moTa: string,
-    giaTien: number,
-    mayGiat: boolean,
-    banLa: boolean,
-    tivi: boolean,
-    dieuHoa: boolean,
-    wifi: boolean,
-    bep: boolean,
-    doXe: boolean,
-    hoBoi: boolean,
-    banUi: boolean,
-    maViTri: number,
-    hinhAnh: string,
-}
 
 export interface ILocationItem {
     hinhAnh: string,
@@ -32,45 +11,3 @@ export interface ILocationItem {
     tenViTri: string,
     tinhThanh: string
 }
-
-export interface IValuesLogin {
-    email: string,
-    password: string,
-}
-
-export interface IValues extends IValuesLogin {
-    checkPassword: string,
-    user: string,
-    name: string,
-    email: string,
-    phone: string,
-    birthday: string,
-    role: string,
-    gender: boolean,
-}
-export interface IProfile extends IValuesLogin {
-    id: number,
-    name: string,
-    phone:string,
-    birthday: string,
-    avatar: string,
-    gender: boolean,
-    role:string,
-}
-
-
-export interface IValueUpdate{ 
-    name: string,
-    email: string,
-    phone: string,
-    birthday: string,
-    role: string,
-    gender: boolean,
-}
-
-
-export const regex = {
-    nameByVietnamese: /^[a-z A-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹý\\s]+$/,
-    password: /^.*(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^& "]).*$/,
-}
-

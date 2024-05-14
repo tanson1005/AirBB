e6cca4db376813e300af5c832af0e490edbde4a1
tanson <tanson01657715766@gmail.com>
2024-05-14 10:03:45 +0700

    create and code feature/SkeletonC 14/5 by devSon

--- src/Template/Home-template/HomeTemplate.tsx
+++ src/Template/Home-template/HomeTemplate.tsx
@@ -3,12 +3,14 @@ import {Outlet} from 'react-router-dom'
 //components
 import HeaderHome from "../../Components/Header-home/HeaderHome"
 import FooterHome from "../../Components/Footer-home/FooterHome"
+import {SkeletonC, SkeletonListRoom} from "../../Components/Skeleton/Skeleton"
+
 
 function HomeTemplate() {
     return (
       <div>
           <HeaderHome/>
-          <Suspense >
+          <Suspense fallback={location.pathname === '/' ? <SkeletonC/> : <SkeletonListRoom/>}>
               <Outlet />
           </Suspense>
           <FooterHome/>

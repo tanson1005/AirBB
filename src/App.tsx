20d6125c147508b044e8ef71449ffb3a0338a891
tanson <tanson01657715766@gmail.com>
2024-05-23 21:45:54 +0700

    Create and code templatesAdmin-ManageUser 23/5 by devSon

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,45 @@
-import { Fragment, lazy } from 'react' // Import Fragment from 'react'
+import { Fragment, lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//templates
+// Templates
 import HomeTemplate from './Template/Home-template/HomeTemplate';
-import DetailTemplate from './Template/Detail-template/DetailTemplate'
-import RoomTemplate from './Template/Room-template/RoomTemplate'
-import RegisterTemplate from './Template/Register-template/RegisterTemplate'
-import AdminTemplate from './Template/Admin-template/AdminTemplate'
-//Lazy
-const HomePage = lazy(() => { return import('./Pages/Home-page/HomePage') })
-const RoomList = lazy(() => { return import('./Pages/Room-list/RoomList') })
-const PersonalInformation = lazy(()=>{return import('./Pages/Personal-information/PersonalInformation')})
-const Detail = lazy(()=>{return import('./Pages/Detail-room/DetailRoom')})
-const Login = lazy(() => { return import('./Pages/Login/Login') })
-const Register = lazy(()=>{return import('./Pages/Register/Register')})
+import DetailTemplate from './Template/Detail-template/DetailTemplate';
+import RoomTemplate from './Template/Room-template/RoomTemplate';
+import RegisterTemplate from './Template/Register-template/RegisterTemplate';
+import AdminTemplate from './Template/Admin-template/AdminTemplate';
+// Lazy loading components
+const HomePage = lazy(() => import('./Pages/Home-page/HomePage'));
+const RoomList = lazy(() => import('./Pages/Room-list/RoomList'));
+const PersonalInformation = lazy(() => import('./Pages/Personal-information/PersonalInformation'));
+const Detail = lazy(() => import('./Pages/Detail-room/DetailRoom'));
+const Login = lazy(() => import('./Pages/Login/Login'));
+const Register = lazy(() => import('./Pages/Register/Register'));
+const ManageUser = lazy(() => import('./Components/Admin/ManageUser'));
 
 function App() {
   return (
     <Fragment>
       <BrowserRouter>
-        <Routes>
-          <Route path='' element={<HomeTemplate />}>
-            <Route path=':idLocation' element={<RoomList />}></Route>
-            <Route path='' element={<HomePage />}></Route>
-          </Route>
-          <Route path='Detail' element={<DetailTemplate />}>
-            <Route path='profile' element={<PersonalInformation/>}></Route>
-          </Route>
-          <Route path='room' element={<RoomTemplate />}>
-            <Route path=':idDetail' element={<Detail />}></Route>
-          </Route>
-          <Route path='auth' element={<RegisterTemplate />}>
-            <Route path='login' element={<Login/>}></Route>
-            <Route path='register' element={<Register/>}></Route>
-          </Route>
-          <Route path='@@admin' element={<AdminTemplate />}></Route>
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='' element={<HomeTemplate />}>
+              <Route path=':idLocation' element={<RoomList />} />
+              <Route path='' element={<HomePage />} />
+            </Route>
+            <Route path='Detail' element={<DetailTemplate />}>
+              <Route path='profile' element={<PersonalInformation />} />
+            </Route>
+            <Route path='room' element={<RoomTemplate />}>
+              <Route path=':idDetail' element={<Detail />} />
+            </Route>
+            <Route path='auth' element={<RegisterTemplate />}>
+              <Route path='login' element={<Login />} />
+              <Route path='register' element={<Register />} />
+            </Route>
+            <Route path='@@admin' element={<AdminTemplate />}>
+              <Route path='user' element={<ManageUser />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </Fragment>
   );

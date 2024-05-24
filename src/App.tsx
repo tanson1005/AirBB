360721d0e97686bc51d9251c4fd9f54c6c3d871c
tanson <tanson01657715766@gmail.com>
2024-05-24 10:44:42 +0700

    deploy-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,57 @@
 import { Fragment, lazy, Suspense } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 // Templates
 import HomeTemplate from './Template/Home-template/HomeTemplate';
 import DetailTemplate from './Template/Detail-template/DetailTemplate';
 import RoomTemplate from './Template/Room-template/RoomTemplate';
 import RegisterTemplate from './Template/Register-template/RegisterTemplate';
 import AdminTemplate from './Template/Admin-template/AdminTemplate';
-import HomePage from './Pages/Home-page/HomePage';
+
 // Lazy loading components
-//user
-// const HomePage = lazy(() => import('./Pages/Home-page/HomePage'));
+// User
+const HomePage = lazy(() => import('./Pages/Home-page/HomePage'));
 const RoomList = lazy(() => import('./Pages/Room-list/RoomList'));
 const PersonalInformation = lazy(() => import('./Pages/Personal-information/PersonalInformation'));
 const Detail = lazy(() => import('./Pages/Detail-room/DetailRoom'));
 const Login = lazy(() => import('./Pages/Login/Login'));
 const Register = lazy(() => import('./Pages/Register/Register'));
-//admin
+
+// Admin
 const ManageUser = lazy(() => import('./Components/Admin/ManageUser'));
-const ManageRoom = lazy(()=>{return import('./Components/Admin/ManageRoom')})
-const ManageBookedRoom = lazy(()=>{return import('./Components/Admin/ManageBookedRoom')})
-const ManageLocation = lazy(()=>{return import('./Components/Admin/ManageLocation')})
+const ManageRoom = lazy(() => import('./Components/Admin/ManageRoom'));
+const ManageBookedRoom = lazy(() => import('./Components/Admin/ManageBookedRoom'));
+const ManageLocation = lazy(() => import('./Components/Admin/ManageLocation'));
 
 function App() {
   return (
     <Fragment>
-      <BrowserRouter>
+      <Router>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route path='' element={<HomeTemplate />}>
-              <Route path='list/:idLocation' element={<RoomList />} />
-              <Route path='' element={<HomePage />} />
+            <Route path="/" element={<HomeTemplate />}>
+              <Route path=":idLocation" element={<RoomList />} />
+              <Route path="/" element={<HomePage />} />
             </Route>
-            <Route path='Detail' element={<DetailTemplate />}>
-              <Route path='profile' element={<PersonalInformation />} />
+            <Route path="detail" element={<DetailTemplate />}>
+              <Route path="profile" element={<PersonalInformation />} />
             </Route>
-            <Route path='room' element={<RoomTemplate />}>
-              <Route path=':idDetail' element={<Detail />} />
+            <Route path="room" element={<RoomTemplate />}>
+              <Route path=":idDetail" element={<Detail />} />
             </Route>
-            <Route path='auth' element={<RegisterTemplate />}>
-              <Route path='login' element={<Login />} />
-              <Route path='register' element={<Register />} />
+            <Route path="auth" element={<RegisterTemplate />}>
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
             </Route>
-            <Route path='@@admin' element={<AdminTemplate />}>
-              <Route path='user' element={<ManageUser />} />
-              <Route path='roomdetail' element={<ManageRoom/>}></Route>
-              <Route path='booked' element={<ManageBookedRoom/>}></Route>
-              <Route path='location' element={<ManageLocation/>}></Route>
+            <Route path="@@admin" element={<AdminTemplate />}>
+              <Route path="user" element={<ManageUser />} />
+              <Route path="roomdetail" element={<ManageRoom />} />
+              <Route path="booked" element={<ManageBookedRoom />} />
+              <Route path="location" element={<ManageLocation />} />
             </Route>
           </Routes>
         </Suspense>
-      </BrowserRouter>
+      </Router>
     </Fragment>
   );
 }

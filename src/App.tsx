0d8787a9db63624905b528287f57fa2479163919
tanson <tanson01657715766@gmail.com>
2024-05-24 11:12:36 +0700

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Fragment, lazy, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Templates
@@ -10,49 +10,47 @@ import AdminTemplate from './Template/Admin-template/AdminTemplate';
 
 // Lazy loading components
 // User
-const HomePage = lazy(() => import('./Pages/Home-page/HomePage'));
-const RoomList = lazy(() => import('./Pages/Room-list/RoomList'));
-const PersonalInformation = lazy(() => import('./Pages/Personal-information/PersonalInformation'));
-const Detail = lazy(() => import('./Pages/Detail-room/DetailRoom'));
-const Login = lazy(() => import('./Pages/Login/Login'));
-const Register = lazy(() => import('./Pages/Register/Register'));
+const HomePage = React.lazy(() => import('./Pages/Home-page/HomePage'));
+const RoomList = React.lazy(() => import('./Pages/Room-list/RoomList'));
+const PersonalInformation = React.lazy(() => import('./Pages/Personal-information/PersonalInformation'));
+const Detail = React.lazy(() => import('./Pages/Detail-room/DetailRoom'));
+const Login = React.lazy(() => import('./Pages/Login/Login'));
+const Register = React.lazy(() => import('./Pages/Register/Register'));
 
 // Admin
-const ManageUser = lazy(() => import('./Components/Admin/ManageUser'));
-const ManageRoom = lazy(() => import('./Components/Admin/ManageRoom'));
-const ManageBookedRoom = lazy(() => import('./Components/Admin/ManageBookedRoom'));
-const ManageLocation = lazy(() => import('./Components/Admin/ManageLocation'));
+const ManageUser = React.lazy(() => import('./Components/Admin/ManageUser'));
+const ManageRoom = React.lazy(() => import('./Components/Admin/ManageRoom'));
+const ManageBookedRoom = React.lazy(() => import('./Components/Admin/ManageBookedRoom'));
+const ManageLocation = React.lazy(() => import('./Components/Admin/ManageLocation'));
 
 function App() {
   return (
-    <Fragment>
+    <Suspense fallback={<div>Loading...</div>}>
       <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<HomeTemplate />}>
-              <Route path=":idLocation" element={<RoomList />} />
-              <Route path="/" element={<HomePage />} />
-            </Route>
-            <Route path="detail" element={<DetailTemplate />}>
-              <Route path="profile" element={<PersonalInformation />} />
-            </Route>
-            <Route path="room" element={<RoomTemplate />}>
-              <Route path=":idDetail" element={<Detail />} />
-            </Route>
-            <Route path="auth" element={<RegisterTemplate />}>
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
-            </Route>
-            <Route path="@@admin" element={<AdminTemplate />}>
-              <Route path="user" element={<ManageUser />} />
-              <Route path="roomdetail" element={<ManageRoom />} />
-              <Route path="booked" element={<ManageBookedRoom />} />
-              <Route path="location" element={<ManageLocation />} />
-            </Route>
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<HomeTemplate />}>
+            <Route path=":idLocation" element={<RoomList />} />
+            <Route path="/" element={<HomePage />} />
+          </Route>
+          <Route path="detail" element={<DetailTemplate />}>
+            <Route path="profile" element={<PersonalInformation />} />
+          </Route>
+          <Route path="room" element={<RoomTemplate />}>
+            <Route path=":idDetail" element={<Detail />} />
+          </Route>
+          <Route path="auth" element={<RegisterTemplate />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+          <Route path="@@admin" element={<AdminTemplate />}>
+            <Route path="user" element={<ManageUser />} />
+            <Route path="roomdetail" element={<ManageRoom />} />
+            <Route path="booked" element={<ManageBookedRoom />} />
+            <Route path="location" element={<ManageLocation />} />
+          </Route>
+        </Routes>
       </Router>
-    </Fragment>
+    </Suspense>
   );
 }
 

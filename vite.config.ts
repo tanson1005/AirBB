4007fd8fcb5f4dc447950f7eb2395617b333536c
tanson <tanson01657715766@gmail.com>
2024-06-02 10:33:06 +0700

    deploy-14

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,40 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4000,
+  },
   resolve:{
     alias:{
       src: '/src',
       public: '/public'
     }
   },
-
-  build:{
-    rollupOptions:{
-      external: ["copy-to-clipboard"]
-    }
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment'
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      '@emotion/react',
+      '@emotion/styled',
+      '@reduxjs/toolkit',
+      'axios',
+      'formik',
+      'date-fns',
+      'yup',
+      '@mui/material',
+      '@mui/x-data-grid',
+      '@mui/x-date-pickers',
+      'antd',
+      'react-date-range',
+      'react-redux',
+      'sweetalert',
+      'swiper'
+    ]
   }
 })
